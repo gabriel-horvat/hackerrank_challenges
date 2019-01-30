075f7ce4d4f191fb1189b3b4a9a84a99cb3f1c5e
Gabe Horvat <realgabrielhorvat@gmail.com>
2019-01-30 15:38:54 +0100

    add different version using fetch

--- countCountries.js
+++ countCountries.js
@@ -23,3 +23,22 @@ req.send(null);
 }
 
 // using fetch:
+function countCountries(str, p) {
+
+   let url= ("https://jsonmock.hackerrank.com/api/countries/search?name=" + str);
+    var arr=[];
+ fetch(url)
+ .then(function(response) {
+   return response.json();
+  })
+ .then(function(jsonResponse) {
+  for (i=0; i<jsonResponse.data.length; i++) {
+     if(jsonResponse.data[i].population > p){
+    arr.push(jsonResponse.data[i].name)
+    };
+
+  }
+    print (arr.length);
+ });
+
+ }
